f93c7d59021675c05ced8edf841332cc66826808
AJ <aj@ajnadel.com>
2016-03-24 14:43:44 -0700

    combine ir and led code; fix mistakes

--- TeensyLightIRSensor.ino
+++ TeensyLightIRSensor.ino
@@ -1,5 +1,6 @@
 #include <Adafruit_NeoPixel.h>
 #include <avr/power.h>
+#include <FlexCAN.h>
 
 #define PIN 6
 #define IR_ANALOG 0
@@ -21,9 +22,8 @@ int infraredTicker = 0;
 int latestInfraredValue = 0;
 unsigned int lastInfraredSampleEpoch = 0;
 
-FlexCAN pixelBus = FlexCAN(1000000);
+FlexCAN canBus = FlexCAN(1000000);
 const int pixelCanID = 0x608;
-FlexCAN irBus = FlexCAN(1000000);
 const int irCanID = 0x611;
 
 void setup() {
@@ -35,60 +35,63 @@ void setup() {
   R = 0;
   G = 0;
   B = 0;
-  pixelBus.begin();
-  irBus.begin();
+  canBus.begin();
 }
 
 void loop() {
   takeInfraredValues();
-  if(pixelBus.available() > 0){
+  if(canBus.available() > 0){
     CAN_message_t rxmsg;
 
-    if (pixelBus.read(rxmsg)) {
+    if (canBus.read(rxmsg)) {
       uint8_t * data = (uint8_t *) malloc(8);
-      if (rxmsg.id == canID){
+      if (rxmsg.id == pixelCanID){
         memcpy(data, rxmsg.buf, 8);
+        mode = data[6] + (data[7] << 8);
+        progress = data[4] + (data[5] << 8);
+        R = data[2];
+        G = data[1];
+        B = data[0];
+        Serial.print(mode);
+        Serial.print("\t");
+        Serial.print(progress);
+        Serial.print("\t");
+        Serial.print(R);
+        Serial.print("\t");
+        Serial.print(G);
+        Serial.print("\t");
+        Serial.print(B);
+        Serial.println();
       }
-      mode = data[6] + (data[7] << 8);
-      progress = data[4] + (data[5] << 8);
-      R = data[2];
-      G = data[1];
-      B = data[0];
-      Serial.print(mode);
-      Serial.print("\t");
-      Serial.print(progress);
-      Serial.print("\t");
-      Serial.print(R);
-      Serial.print("\t");
-      Serial.print(G);
-      Serial.print("\t");
-      Serial.print(B);
-      Serial.println();
-      delete data;
-    }
-  }
-  
-  if(irBus.available() > 0){
-    CAN_message_t rxmsg;
+      else if (rxmsg.id == irCanID) {
+        CAN_message_t txmsg;
 
-    if (irBus.read(rxmsg)) {
-      CAN_message_t txmsg;
+        txmsg.id = irCanID;
+        txmsg.len = 8;
 
-      txmsg.id = irCanID;
-      txmsg.len = 8;
+        data[0] = latestInfraredValue & 0xff;
+        data[1] = (latestInfraredValue >> 8) & 0xff;
+        data[2] = (latestInfraredValue >> 16) & 0xff;
+        data[3] = (latestInfraredValue >> 24) & 0xff;
 
-      data[0] = latestInfraredValue & 0xff;
-      data[1] = (latestInfraredValue >> 8) & 0xff;
-      data[2] = (latestInfraredValue >> 16) & 0xff;
-      data[3] = (latestInfraredValue >> 24) & 0xff;
+        data[4] = 0; // mode
 
-      memcpy(txmsg.buf, data, 8);
-      irCanID.write(txmsg);
+        for(int k = 5; k < 8; k++){
+          data[k] = 0;
+        }
+
+        memcpy(txmsg.buf, data, 8);
+        canBus.write(txmsg);
+      }
       
       delete data;
     }
   }
 
+  lights();
+}
+
+void lights(){
   switch(mode){
     default:
       solid();
